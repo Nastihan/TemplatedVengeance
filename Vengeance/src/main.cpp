ca6895e2767938a6d6703baa8b3b6540f775d1bb
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-11 17:08:23 +0330

    starting point

--- Vengeance/src/main.cpp
+++ Vengeance/src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+
+
+template <typename T>
+T Max(T a, T b)
+{
+	return b < a ? a : b;
+}
+
 int main(){
 
-	std::cout << "hello" << std::endl;
+	auto value1 = "a";
+	auto value2 = "b";
+
+	std::cout << "Max : " << Max(value1, value2) << std::endl;
 
 }
