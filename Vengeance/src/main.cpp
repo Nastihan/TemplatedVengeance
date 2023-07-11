98bd0549737b8da4c41961fde7020e9790451358
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-11 17:31:08 +0330

    compiler cannot deduce default argument alone

--- Vengeance/src/main.cpp
+++ Vengeance/src/main.cpp
@@ -8,11 +8,22 @@ T Max(T a, T b)
 	return b < a ? a : b;
 }
 
+template <typename T>
+T Foo(T t = 10)
+{
+	std::cout << "calling foo" << std::endl;
+	return t;
+}
+
 int main(){
 
 	auto value1 = "a";
 	auto value2 = "b";
 
+	
+
 	std::cout << "Max : " << Max(value1, value2) << std::endl;
+
+	std::cout << Foo() << std::endl;
 
 }
